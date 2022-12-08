4af2368637197789c58b33eecdc33b95555c3600
Na Lin <lna483018@gmail.com>
2022-12-08 11:04:30 -0500

    build up db with candy model

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,33 @@
-const Sequelize = require('sequelize');
-const db = require('../database');
+const Sequelize = require("sequelize");
+const db = require("../database");
 
-module.exports = db.define('candy', {
+const defaultImageUrl = "";
+
+module.exports = db.define("candy", {
   // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  quantity: {
+    type: Sequelize.NUMBER,
+    validate: {
+      max: 10,
+      min: 0,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "/bg.jpg",
+  },
 });
