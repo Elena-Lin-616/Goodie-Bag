33821a4ff263d4c9fdea445bede213be0f7b393b
Na Lin <lna483018@gmail.com>
2022-12-08 11:33:21 -0500

    add router to react app

--- app/Root.js
+++ app/Root.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import CandiesList from "./components/CandiesList";
+import Navbar from "./components/NavBar";
 const Root = () => {
   return (
     <div>
-      <nav>Goodie Bag</nav>
+      <Navbar />
       <main>
         <h1>Welcome to the Goodie Bag!</h1>
         <p>What a nice home page for your goodies!</p>
+        <Routes>
+          <Route path="/candies" element={<CandiesList />} />
+        </Routes>
       </main>
     </div>
   );
--- app/main.js
+++ app/main.js
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import Root from "./Root";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { BrowserRouter } from "react-router-dom";
 const root = createRoot(document.getElementById("main"));
 
 root.render(
   <Provider store={store}>
-    <Root />
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
   </Provider>
 );
