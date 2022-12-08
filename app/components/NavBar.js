0231ce0fbc686b1efece3732917ee8e16e5ba71b
Na Lin <lna483018@gmail.com>
2022-12-08 11:34:31 -0500

    build up navbar component to navigate between home and candies page

---
+++ app/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Navbar() {
+  return (
+    <nav>
+      <h1>Goodie Bag</h1>
+      <Link to="/">Home</Link>
+      <Link to="/candies">Candies</Link>
+    </nav>
+  );
+}
+export default Navbar;
